b617de882728877ca665dc347d203177ed1621d0
dhruv-repo-1959 <57010741+dhruv-repo-1959@users.noreply.github.com>
2020-05-11 15:40:45 +0530

    length validation

--- Test/src/test/LeapYearTest.java
+++ Test/src/test/LeapYearTest.java
@@ -9,32 +9,47 @@ import main.LeapYear;
 public class LeapYearTest {
 
 	@Test
-	public void test2000LeapYear() {
+	public void test2000LeaveYear() {
 		assertEquals(true, LeapYear.isLeapYear(2000));
 	}
 	
 	@Test
-	public void test1900LeapYearNot() {
+	public void test1900LeaveYearNot() {
 		assertEquals(false, LeapYear.isLeapYear(1900));
 	}
 	
 	@Test
-	public void test2100LeapYearNot() {
+	public void test2100LeaveYearNot() {
 		assertEquals(false, LeapYear.isLeapYear(2100));
 	}
 	
 	@Test
-	public void test2008LeapYear() {
+	public void test2008LeaveYear() {
 		assertEquals(true, LeapYear.isLeapYear(2008));
 	}
 
 	@Test
-	public void test2019LeapYearNot() {
+	public void test2019LeaveYearNot() {
 		assertEquals(false, LeapYear.isLeapYear(2019));
 	}
 	
 	@Test
-	public void testLeapYearZero() {
+	public void testLeaveYearZero() {
 		assertEquals(false, LeapYear.isLeapYear(0));
+	}
+	
+	@Test
+	public void test19000LeaveYearNot() {
+		assertEquals(false, LeapYear.isLeapYear(19000));
+	}
+	
+	@Test
+	public void test123LeaveYearNot() {
+		assertEquals(false, LeapYear.isLeapYear(123));
+	}
+	
+	@Test
+	public void test12LeaveYearNot() {
+		assertEquals(false, LeapYear.isLeapYear(12));
 	}
 }
