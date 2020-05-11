efb23e8de29cf9bd93c329032edf0d4ebb42f45b
dhruv-repo-1959 <57010741+dhruv-repo-1959@users.noreply.github.com>
2020-05-11 12:26:51 +0530

    more test cases

--- Test/src/test/LeapYearTest.java
+++ Test/src/test/LeapYearTest.java
@@ -9,13 +9,28 @@ import main.LeapYear;
 public class LeapYearTest {
 
 	@Test
-	public void testLeaveYear() {
-		assertEquals(true, LeapYear.isLeapYear(2400));
+	public void test2000LeaveYear() {
+		assertEquals(true, LeapYear.isLeapYear(2000));
+	}
+	
+	@Test
+	public void test1900LeaveYearNot() {
+		assertEquals(false, LeapYear.isLeapYear(1900));
+	}
+	
+	@Test
+	public void test2100LeaveYearNot() {
+		assertEquals(false, LeapYear.isLeapYear(2100));
+	}
+	
+	@Test
+	public void test2008LeaveYear() {
+		assertEquals(true, LeapYear.isLeapYear(2008));
 	}
 
 	@Test
-	public void testLeaveYearNot() {
-		assertEquals(false, LeapYear.isLeapYear(1900));
+	public void test2019LeaveYearNot() {
+		assertEquals(false, LeapYear.isLeapYear(2019));
 	}
 	
 	@Test
